022295194665f58b9b037a6ba8aee7d424f1de74
MadurangaNamal <maduranga.wimalarathne95@gmail.com>
2022-11-06 14:32:40 +0530

    init

---
+++ src/test/java/com/maduranga/awsdemoapp/AwsDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maduranga.awsdemoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
